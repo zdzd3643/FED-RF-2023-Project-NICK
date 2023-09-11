8ecb5ce1c38394d8272e6ddce66933a8792a836e
zdzd3643 <zdzd3643@gmail.com>
2023-09-11 09:46:06 +0900

    ff

--- 800.개인프로젝트/01.웹프로젝트/03.구현소스/js/main.js
+++ 800.개인프로젝트/01.웹프로젝트/03.구현소스/js/main.js
@@ -1,4 +1,32 @@
+// GS25 PJ 메인 JS - main.js
 
-// 공통 DOM 선택함수
+// 요소 선택함수//////////////
 const qs = x => document.querySelector(x);
 const qsa = x => document.querySelectorAll(x);
+
+// 로딩구역 호출설정
+window.addEventListener('DOMContentLoaded',loadFn);
+////////////// 로딩구역 함수 ///////////////////
+///////////////////////////////////////////////
+function loadFn(){
+    // 로딩확인
+    console.log('로딩완료!');
+
+    // 부드러운 스크롤 적용 /////////////////
+    startSS();
+
+const jsearch = qs('#jsearchBar');
+
+    console.log("요소확인:",jsearch);
+
+const Data = qs('.jsearch.value');
+
+    console.log("입력된값:",Data)
+
+var validatFn = function (txt) {
+
+
+};
+
+}; ///////////////// loadFn ///////////////////
+///////////////////////////////////////////////
