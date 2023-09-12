7cc707665bf9585f326e86cd2f1e4229830a1801
zdzd3643 <zdzd3643@gmail.com>
2023-09-12 18:35:20 +0900

    ff

--- 800.개인프로젝트/01.웹프로젝트/03.구현소스/js/main.js
+++ 800.개인프로젝트/01.웹프로젝트/03.구현소스/js/main.js
@@ -4,10 +4,21 @@
 const qs = x => document.querySelector(x);
 const qsa = x => document.querySelectorAll(x);
 
-// 로딩구역 호출설정
+// addEvent 함수
+// ele - 요소, evt - 이벤트, fn - 함수
+const addEvt = 
+(ele, evt, fn) => ele.addEventListener(evt, fn);
+
+// HTML태그 로딩후 loadFn함수 호출! ///
 window.addEventListener('DOMContentLoaded',loadFn);
-////////////// 로딩구역 함수 ///////////////////
-///////////////////////////////////////////////
+
+let clickSts = 0;
+const TIME_SLIDE = 400;
+
+/****************************************** 
+    함수명: loadFn
+    기능: 로딩 후 버튼 이벤트 및 기능구현
+******************************************/
 function loadFn(){
     // 로딩확인
     console.log('로딩완료!');
@@ -15,13 +26,11 @@ function loadFn(){
     // 부드러운 스크롤 적용 /////////////////
     startSS();
 
-const jsearch = qs('#jsearchBar');
+    const abtn = qsa('.abtn');
+    const slide = qs('#slide');
+    const indicate = qsa('.indicate li');
+    
 
-    console.log("요소확인:",jsearch);
-
-const Data = qs('.jsearch.value');
-
-    console.log("입력된값:",Data)
 
 }; ///////////////// loadFn ///////////////////
 ///////////////////////////////////////////////
