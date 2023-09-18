61a6e41d1969577b63a318c50a39817df5a58c8c
Tom B. Parker <tombap8@hotmail.com>
2023-09-18 15:25:11 +0900

    탐쌤

--- 800.개인프로젝트/01.웹프로젝트/03.구현소스/js/main.js
+++ 800.개인프로젝트/01.웹프로젝트/03.구현소스/js/main.js
@@ -1,149 +1,151 @@
 // GS25 PJ 메인 JS - main.js
 
 // 요소 선택함수//////////////
-const qs = x => document.querySelector(x);
-const qsa = x => document.querySelectorAll(x);
+const qs = (x) => document.querySelector(x);
+const qsa = (x) => document.querySelectorAll(x);
 
 // addEvent 함수
 // ele - 요소, evt - 이벤트, fn - 함수
-const addEvt = 
-(ele, evt, fn) => ele.addEventListener(evt, fn);
+const addEvt = (ele, evt, fn) => ele.addEventListener(evt, fn);
 
 // HTML태그 로딩후 loadFn함수 호출! ///
-window.addEventListener('DOMContentLoaded',loadFn);
+window.addEventListener("DOMContentLoaded", loadFn);
 
 let clickSts = 0;
-const TIME_SLIDE = 0;
+const TIME_SLIDE = 400;
 
 /****************************************** 
     함수명: loadFn
     기능: 로딩 후 버튼 이벤트 및 기능구현
 ******************************************/
-function loadFn(){
-    // 로딩확인
-    console.log('로딩완료!');
+function loadFn() {
+  // 로딩확인
+  console.log("로딩완료!");
 
-    // 부드러운 스크롤 적용 /////////////////
-    startSS();
+  // 부드러운 스크롤 적용 /////////////////
+  startSS();
 
-    const abtn = qsa('.abtn');
-    const slide = qs('#slide');
-    const indicate = qsa('.indicate li');
-    
-    // console.log('대상',abtn,slide,indicate);
+  slideFn(qs('.slide-wrap-box'));
+//   slideFn(qs('.ins-banner'));
 
-    slide.querySelectorAll('li').forEach((ele,idx)=>ele.setAttribute('data-seq',idx));
 
-    abtn.forEach(ele=>addEvt(ele,'click',goSlide));
+  ////// 슬라이드 공용 함수 ////////////
+  function slideFn(tgEle) {
+    // tgEle - 타겟요소
 
-    function goSlide(){
-        if(clickSts) return;
-        clickSts=1;
-        setTimeout(() => clickSts=0, TIME_SLIDE);
+    const abtn = tgEle.querySelectorAll(".abtn");
+    const slide = tgEle.querySelector('.slide');
+    const indicate = tgEle.querySelectorAll(".indicate li");
 
-        stsI = 0;
+    console.log('대상',abtn,slide,indicate);
 
-        if(stsI) clearAuto();
+    slide
+      .querySelectorAll("li")
+      .forEach((ele, idx) => ele.setAttribute("data-seq", idx));
 
-        console.log('나야나!',this,this.classList.contains('ab2'));
+    abtn.forEach((ele) => addEvt(ele, "click", goSlide));
 
-        let isRight = this.classList.contains('ab2');
+    function goSlide(e) {
+        e.preventDefault();
+      if (clickSts) return;
+      clickSts = 1;
+      setTimeout(() => (clickSts = 0), TIME_SLIDE);
 
-        let eachOne = slide.querySelectorAll('li');
+      stsI = 0;
 
-        if(isRight){
-            rigthSlide();
-        }
-        else{
-            slide.insertBefore(
-                eachOne[eachOne.length-1], eachOne[0]);
-            slide.style.left = '-100%';
-            slide.style.transition = 'none';
+      if (stsI) clearAuto();
 
-            setTimeout(() => {
-                slide.style.left = '0';
+      console.log("나야나!", this, this.classList.contains("ab2"));
 
-                slide.style.transition = 
-                    TIME_SLIDE+'ms ease-in-out';
-            }, 0);
-        } ////////// else //////////
+      let isRight = this.classList.contains("ab2");
 
-        chgIndicate(isRight);
+      let eachOne = slide.querySelectorAll("li");
 
-        clearAuto();
-
-    } ////////////// goSlide 함수 //////////////
-
-    function chgIndicate(isRight){
-        let nowSeq =
-        slide.querySelectorAll('li')[isRight?1:0].getAttribute('data-seq');
-
-        console.log('현재슬라이드 순번:',nowSeq);
-        
-        indicate.forEach((ele,idx)=>{
-            if(idx==nowSeq) ele.classList.add('on');
-            else ele.classList.remove('on');
-        }); ////////// forEach //////////
-
-    } //////// chgIndicat 함수 ///////////////
-
-    function rigthSlide(){
-        slide.style.left = '-100%';
-        slide.style.transition =
-            TIME_SLIDE+'ms ease-in-out';
+      if (isRight) {
+        rigthSlide();
+      } else {
+        slide.insertBefore(eachOne[eachOne.length - 1], eachOne[0]);
+        slide.style.left = "-100%";
+        slide.style.transition = "none";
 
         setTimeout(() => {
-            slide.appendChild(
-                slide.querySelectorAll('li')[0]);
-            slide.style.left = '0';
-            slide.style.transition = 'none';
-        }, TIME_SLIDE);
+          slide.style.left = "0";
+
+          slide.style.transition = TIME_SLIDE + "ms ease-in-out";
+        }, 0);
+      } ////////// else //////////
+
+      chgIndicate(isRight);
+
+      clearAuto();
+    } ////////////// goSlide 함수 //////////////
+
+    function chgIndicate(isRight) {
+      let nowSeq = slide
+        .querySelectorAll("li")
+        [isRight ? 1 : 0].getAttribute("data-seq");
+
+      console.log("현재슬라이드 순번:", nowSeq);
+
+      indicate.forEach((ele, idx) => {
+        if (idx == nowSeq) ele.classList.add("on");
+        else ele.classList.remove("on");
+      }); ////////// forEach //////////
+    } //////// chgIndicat 함수 ///////////////
+
+    function rigthSlide() {
+      slide.style.left = "-100%";
+      slide.style.transition = TIME_SLIDE + "ms ease-in-out";
+
+      setTimeout(() => {
+        slide.appendChild(slide.querySelectorAll("li")[0]);
+        slide.style.left = "0";
+        slide.style.transition = "none";
+      }, TIME_SLIDE);
     } ////////// rightSlide 함수 ///////////
 
     let autoI;
 
     let autoT;
 
-    function slideAuto(){
-        autoI = setInterval(() => {
-            rigthSlide();
-            chgIndicate(1);
-        }, 3000);
+    function slideAuto() {
+      autoI = setInterval(() => {
+        rigthSlide();
+        chgIndicate(1);
+      }, 3000);
     } //////////// slideAuto 함수 ///////////
 
     slideAuto();
 
-    function clearAuto(){
-        console.log('멈춤!');
+    function clearAuto() {
+      console.log("멈춤!");
 
-        clearInterval(autoI);
-        clearTimeout(autoT);
+      clearInterval(autoI);
+      clearTimeout(autoT);
 
-        autoT = setTimeout(slideAuto,3000);
+      autoT = setTimeout(slideAuto, 3000);
     } ///////// clearAuto 함수 //////////////
-
-}; ///////////////// loadFn ///////////////////
+  } ///////////// slideFn 함수 ////////////////
+} ///////////////// loadFn ///////////////////
 ///////////////////////////////////////////////
 
-var 나야나 = document.getElementById('mv').src;
+var 나야나 = document.getElementById("mv").src;
 
-console.log("내꺼얌:",나야나);
+console.log("내꺼얌:", 나야나);
 
-document.getElementById('mv').src = "https://www.youtube.com/embed/cjH6sAgIb_k";
+document.getElementById("mv").src = "https://www.youtube.com/embed/cjH6sAgIb_k";
 
-function 바꿔(먹이){
+function 바꿔(먹이) {
+  console.log("영상바꿔", 먹이);
 
-    console.log('영상바꿔',먹이);
-    
-    document.getElementById('mv').src = "https://www.youtube.com/embed/"+먹이+"?autoplay=1";
+  document.getElementById("mv").src =
+    "https://www.youtube.com/embed/" + 먹이 + "?autoplay=1";
 }
-//////////////////////// 바꿔 함수 /////////////////////////    
+//////////////////////// 바꿔 함수 /////////////////////////
 
+function 커짐() {
+  console.log("커지냐");
 
-function 커짐(){
-    
-    console.log('커지냐');
-
-    document.getElementsByClassName('tv').item(0).classList.toggle('on');
+  document.getElementsByClassName("tv").item(0).classList.toggle("on");
 }
 //////////////////// 커짐 함수 //////////////////////////////
