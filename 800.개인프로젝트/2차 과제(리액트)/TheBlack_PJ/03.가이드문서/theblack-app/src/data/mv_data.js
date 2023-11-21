bee6b7d0063f852c70250c55682bc4ffa63ee683
zdzd3643 <zdzd3643@gmail.com>
2023-11-21 16:58:58 +0900

    FF

---
+++ 800.개인프로젝트/2차 과제(리액트)/TheBlack_PJ/03.가이드문서/theblack-app/src/data/mv_data.js
@@ -0,0 +1,24 @@
+// 뮤비 데이터
+export const mvData = {
+  somi:{
+      vsrc:"./images/SOMI_MV.mp4",
+      valt:"전소미 뮤직비디오",
+      tit:"JEONSOMI(전소미)-‘Fast Forward’ M/V"
+  },
+  taeyang:{
+      vsrc:"./images/TAEYANG_MV.mp4",
+      valt:"태양 뮤직비디오",
+      tit:"T태양 - ‘나의 마음에 (Seed)’ M/V"
+  },
+  ziont:{
+      vsrc:"./images/ZIONT_MV.mp4",
+      valt:"자이언티 뮤직비디오",
+      tit:"Zion.T - '선물을 고르며(A Gift!)’ M/V"
+  },
+  taeyang:{
+      vsrc:"./images/BryanChase_MV.mp4",
+      valt:"브라이언 뮤직비디오",
+      tit:"브라이언 체이스 - ‘Raindrop’ M/V"
+  },
+
+} ///////// mvData ////////////
