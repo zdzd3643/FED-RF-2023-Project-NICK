9a2f37faeb0dce3e31bea8efdc7b0a45c58f06ed
zdzd3643 <zdzd3643@gmail.com>
2023-11-21 19:37:12 +0900

    ff

--- 800.개인프로젝트/2차 과제(리액트)/TheBlack_PJ/02.구현소스/theblack-app/src/layout/FooterArea.jsx
+++ 800.개인프로젝트/2차 과제(리액트)/TheBlack_PJ/02.구현소스/theblack-app/src/layout/FooterArea.jsx
@@ -3,7 +3,15 @@
 export function FooterArea(){
   return(
     <>
-    <div id="footer-area">하단임!</div>
+    <div id="footer-area">
+      <footer className="info">
+        <address>
+            Copyright ⓒ THEBLACKLABEL All Rights Reserved.<br/>
+            일반문의 info@theblacklabel.com | 오디션문의 audition@theblacklabel.com <br/>
+            대표 : 박홍준(Teddy)<br/>
+        </address>
+      </footer>
+    </div>
     </>
   )
 } /////////////// FooterArea 컴포넌트 ///////////////
