b6fdc2c4429530450c5d4a6f4f06fdfaa9485f97
zdzd3643 <zdzd3643@gmail.com>
2024-01-12 18:20:00 +0900

    FF

--- 800.개인프로젝트/2차 과제(리액트)/TheBlack_PJ/02.구현소스/theblack-app/src/func/drag_banner.js
+++ 800.개인프로젝트/2차 과제(리액트)/TheBlack_PJ/02.구현소스/theblack-app/src/func/drag_banner.js
@@ -5,77 +5,90 @@ window.jQuery = $;
 require("jquery-ui-dist/jquery-ui");
 require("jquery-ui-touch-punch/jquery.ui.touch-punch");
 
-export function dragBanner() {
-  // 드래그 기능넣기
-  // 대상:
-  // 슬라이드
-  const slide = $(".ats-movie-area");
+export function darggBanner() {
+  const dragSlide = (e) => {
+    const tg = e.target;
+    // console.log(tg);
+
+  // 2. 대상선정
+  // (1) 슬라이드
+  const sldBox = $(tg).parent().siblings(".ats-movie-area");
+  // console.log("현재슬라이드:",sldBox);
+  // (2) 슬라이드 블릿
+  const bindic = $(tg).parent().siblings('.bindic').find('li');
+  // console.log('블릿:',bindic);
+  // (3) 슬라이드 개수
+  const sCnt = sldBox.find('li').length;
+  // console.log('슬라이드개수:', sCnt);
+
   // 커버
   const cover = $(".cover");
   // 블릿
-  const bindic = $(".bindic");
-  //   console.log(indic);
-
+  const indic = $(".bindic li");
+    console.log(indic);
   // 드래그 기능넣기
-  slide.draggable({ axis: "x" });
+  sldBox.draggable({ axis: "x" });
 
   // 드래그가 끝났을때 슬라이드 위치
-  slide.on("dragstop", () => {
+  sldBox.on("dragstop",()=>{
     // 광드래그 막기 커버
-    cover.show();
+  cover.show();
 
     // 비교를 위한 윈도우 가로값
-    let winW = $(window).width();
-    // 현재 슬라이드 left값
-    let pos = slide.offset().left;
-    // 이동차이수 = 슬라이드위치값(양수) - 윈도우가로값
-    let diff = Math.abs(pos) - winW;
-    // 결과해석: 양수->왼쪽으로 이동/음수->오른쪽으로 이동
+  let winW = $(window).width();
+  // 현재 슬라이드 left값
+  let pos = sldBox.offset().left;
+  // 이동차이수 = 슬라이드위치값(양수) - 윈도우가로값
+  let diff = Math.abs(pos) - winW;
+  // 결과해석: 양수->왼쪽으로 이동/음수->오른쪽으로 이동
 
-    // 기준값 : 화면크기를 기준한 부분
-    let gap = winW / 10;
-    // console.log("드래그 멈춤!", pos, winW, diff);
+  // 기준값 : 화면크기를 기준한 부분
+  let gap = winW / 10;
+  // console.log("드래그 멈춤!", pos, winW, diff);
 
-    // 왼쪽으로 이동하기 ///////////////
-    if (diff > gap) {
-      slide.animate({ left: "-200%" }, 800, "easeOutQuint", () => {
-        // 맨앞li 맨뒤로 이동
-        slide
-          .append(slide.find("li").first())
-          // left값 -100%(처음값)
-          .css({ left: "-100%" });
-        // 커버제거
-        cover.hide();
-      });
-    }
-    // 오른쪽으로 이동하기 //////////////
-    else if (diff < -gap) {
-      slide.animate({ left: "0" }, 800, "easeOutQuint", () => {
-        // 맨뒤li 맨앞으로 이동
-        slide
-          .prepend(slide.find("li").last())
-          // left값 -100%(처음값)
-          .css({ left: "-100%" });
-        // 커버제거
-        cover.hide();
-      });
-    }
-    // 제자리로 ////
-    else {
-      slide.animate({ left: "-100%" }, 300, "easeOutQuint", () => {
-        // 커버제거
-        cover.hide();
-      });
-    }
-    
-    // 블릿변경함수호출
-    chgIndic();
-  }); /////////// dragstop /////////
+  // 왼쪽으로 이동하기 ///////////////
+  if (diff > gap) {
+    sldBox.animate({ left: "-200%" }, 800, "easeOutQuint", () => {
+      // 맨앞li 맨뒤로 이동
+      sldBox
+        .append(sldBox.find("li").first())
+        // left값 -100%(처음값)
+        .css({ left: "-100%" });
+      // 커버제거
+      cover.hide();
+    });
+    sNum++;
+    if(sNum>=sCnt) sNum=0;
+    console.log('슬라이드 순번:',sNum);
+  }
+  // 오른쪽으로 이동하기 //////////////
+  else if (diff < -gap) {
+    sldBox.animate({ left: "0%" }, 800, "easeOutQuint", () => {
+      // 맨뒤li 맨앞으로 이동
+      sldBox
+        .prepend(sldBox.find("li").last())
+        // left값 -100%(처음값)
+        .css({ left: "-100%" });
+      // 커버제거
+      cover.hide();
+    });
+    // 슬라이드순번 감소(0보다 작으면 끝번호)
+  sNum--;
+    if(sNum<0) sNum = sCnt-1;
+    console.log('슬라이드 순번:',sNum);
+  }
+  // 제자리로 ////
+  else {
+    sldBox.animate({ left: "-100%" }, 300, "easeOutQuint", () => {
+      // 커버제거
+      cover.hide();
+    });
+  }
 
-  //////// 블릿변경함수 /////////////
-  const chgIndic = () => {
-    bindic.eq(sNum).addClass('on')
-    .siblings().removeClass('on');
-  }; ////////// chgIndic 함수 ///////
+  bindic.eq(sNum).addClass('on')
+  .siblings().removeClass('on');
 
-} /////////// dragBanner 함수 //////////////
+}); /////////// dragstop /////////
+
+} ////////// dragSlide함수 ////////////
+} /////////////
--- 800.개인프로젝트/2차 과제(리액트)/TheBlack_PJ/02.구현소스/theblack-app/src/modules/Banner.jsx
+++ 800.개인프로젝트/2차 과제(리액트)/TheBlack_PJ/02.구현소스/theblack-app/src/modules/Banner.jsx
@@ -5,8 +5,6 @@ import { mvData } from "../data/mv_data";
 
 import "../css/banner.css";
 
-import { useEffect } from "react";
-
 // 제이쿼리 + 제이쿼리UI
 import $ from "jquery";
 import "jquery-ui-dist/jquery-ui";
@@ -30,93 +28,21 @@ export function Banner(){
     let cSts = 0;
     // (3) 슬라이드순번
     let sNum = 0;
-
+    
     const goSlide = (e) => {
+      // 1. 이벤트가 발생한 요소
       const tg = e.target;
-      // console.log(tg);
 
-    // 2. 대상선정
-    // (1) 슬라이드
-    const sldBox = $(tg).parent().siblings(".ats-movie-area");
-    // console.log("현재슬라이드:",sldBox);
-    // (2) 슬라이드 블릿
-    const bindic = $(tg).parent().siblings('.bindic').find('li');
-    // console.log('블릿:',bindic);
-    // (3) 슬라이드 개수
-    const sCnt = sldBox.find('li').length;
-    // console.log('슬라이드개수:', sCnt);
-
-    // 커버
-    const cover = $(".cover");
-    // 블릿
-    const indic = $(".bindic li");
-      console.log(indic);
-    // 드래그 기능넣기
-    sldBox.draggable({ axis: "x" });
-
-    // 드래그가 끝났을때 슬라이드 위치
-    sldBox.on("dragstop",()=>{
-      // 광드래그 막기 커버
-    cover.show();
-
-      // 비교를 위한 윈도우 가로값
-    let winW = $(window).width();
-    // 현재 슬라이드 left값
-    let pos = sldBox.offset().left;
-    // 이동차이수 = 슬라이드위치값(양수) - 윈도우가로값
-    let diff = Math.abs(pos) - winW;
-    // 결과해석: 양수->왼쪽으로 이동/음수->오른쪽으로 이동
-
-    // 기준값 : 화면크기를 기준한 부분
-    let gap = winW / 10;
-    // console.log("드래그 멈춤!", pos, winW, diff);
-
-    // 왼쪽으로 이동하기 ///////////////
-    if (diff > gap) {
-      sldBox.animate({ left: "-200%" }, 800, "easeOutQuint", () => {
-        // 맨앞li 맨뒤로 이동
-        sldBox
-          .append(sldBox.find("li").first())
-          // left값 -100%(처음값)
-          .css({ left: "-100%" });
-        // 커버제거
-        cover.hide();
-      });
-      sNum++;
-      if(sNum>=sCnt) sNum=0;
-      console.log('슬라이드 순번:',sNum);
-    }
-    // 오른쪽으로 이동하기 //////////////
-    else if (diff < -gap) {
-      sldBox.animate({ left: "0%" }, 800, "easeOutQuint", () => {
-        // 맨뒤li 맨앞으로 이동
-        sldBox
-          .prepend(sldBox.find("li").last())
-          // left값 -100%(처음값)
-          .css({ left: "-100%" });
-        // 커버제거
-        cover.hide();
-      });
-      // 슬라이드순번 감소(0보다 작으면 끝번호)
-    sNum--;
-      if(sNum<0) sNum = sCnt-1;
-      console.log('슬라이드 순번:',sNum);
-    }
-    // 제자리로 ////
-    else {
-      sldBox.animate({ left: "-100%" }, 300, "easeOutQuint", () => {
-        // 커버제거
-        cover.hide();
-      });
-    }
-
-    bindic.eq(sNum).addClass('on')
-    .siblings().removeClass('on');
-
-  }); /////////// dragstop /////////
-
-    // 3. 이벤트 설정 및 기능구현
-    // 3-1. 이동버튼 클릭시
+      // 2. 대상선정
+      // (1) 슬라이드
+      const sldBox = $(tg).parent().siblings(".ats-movie-area");
+      // console.log("현재슬라이드:",sldBox);
+      // (2) 슬라이드 블릿
+      const bindic = $(tg).parent().siblings('.bindic').find('li');
+      // console.log('블릿:',bindic);
+      // (3) 슬라이드 개수
+      const sCnt = sldBox.find('li').length;
+      // console.log('슬라이드개수:', sCnt);
 
       // 0. 광클금지 //////////
       if(cSts) return;
@@ -125,7 +51,7 @@ export function Banner(){
       ///////////////////////////
 
       // 1. 오른쪽버튼 여부
-      let isR = $(tg).parent().is('.rar');
+      let isR = $(tg).is('.rar');
       // console.log('버튼클릭!', isR);
 
       // 2. 버튼별분기
@@ -144,20 +70,19 @@ export function Banner(){
           sNum++;
           if(sNum>=sCnt) sNum=0;
       } ////////// if ///////////
-
-      // 2F-2. 왼쪽버튼
-      else{
+      // 2-2. 왼쪽버튼
+      else {
           // 맨뒤li 맨앞으로 이동
-          sldBox.prepend(sldBox.find('li').last())
-          // left값 -100%
-          .css({left:'-200%'})
-          // left값을 0으로 애니메이션
-          .animate({left:'-100%'},A_TM, A_ES);
-
-          // 슬라이드순번 감소(0보다 작으면 끝번호)
-          sNum--;
-          if(sNum<0) sNum = sCnt-1;
-      } ////////// else ///////////
+        sldBox.prepend(sldBox.find('li').last())
+        // left값 -100%
+        .css({left:'-200%'})
+        // left값을 0으로 애니메이션
+        .animate({left:'-100%'},A_TM,A_ES);
+  
+        // 슬라이드순번 감소(0보다 작으면 끝번호)
+        sNum--;
+        if(sNum<0) sNum=sCnt-1;
+      } /////// else /////////
 
       // console.log('슬라이드 순번:',sNum);
 
@@ -167,7 +92,86 @@ export function Banner(){
 
     } ////////// goSlide함수 ////////////
 
-    
+    $(()=>{
+      const sldBox = $(".ats-movie-area");
+      // console.log("현재슬라이드:",sldBox);
+      // (1) 드래그 기능넣기
+      sldBox.draggable({ axis: "x" });
+      
+      // (2) 슬라이드 블릿
+      const bindic = $('.bindic').find('li');
+      // console.log('블릿:',bindic);
+      // (3) 슬라이드 개수
+      const sCnt = sldBox.find('li').length;
+      // console.log('슬라이드개수:', sCnt);
+  
+      // 커버
+      const cover = $(".cover");
+      // 블릿
+
+      // 드래그가 끝났을때 슬라이드 위치
+      sldBox.on("dragstop",()=>{
+        // 광드래그 막기 커버
+      cover.show();
+  
+        // 비교를 위한 윈도우 가로값
+      let winW = $(window).width();
+      // 현재 슬라이드 left값
+      let pos = sldBox.offset().left;
+      // 이동차이수 = 슬라이드위치값(양수) - 윈도우가로값
+      let diff = Math.abs(pos) - winW;
+      // 결과해석: 양수->왼쪽으로 이동/음수->오른쪽으로 이동
+  
+      // 기준값 : 화면크기를 기준한 부분
+      let gap = winW / 10;
+      // console.log("드래그 멈춤!", pos, winW, diff);
+  
+      // 왼쪽으로 이동하기 ///////////////
+      if (diff > gap) {
+        sldBox.animate({ left: "-200%" }, 800, "easeOutQuint", () => {
+          // 맨앞li 맨뒤로 이동
+          sldBox
+            .append(sldBox.find("li").first())
+            // left값 -100%(처음값)
+            .css({ left: "-100%" });
+          // 커버제거
+          cover.hide();
+        });
+        sNum++;
+        if(sNum>=sCnt) sNum=0;
+        // console.log('슬라이드 순번:',sNum);
+      }
+      // 오른쪽으로 이동하기 //////////////
+      else if (diff < -gap) {
+        sldBox.animate({ left: "0%" }, 800, "easeOutQuint", () => {
+          // 맨뒤li 맨앞으로 이동
+          sldBox
+            .prepend(sldBox.find("li").last())
+            // left값 -100%(처음값)
+            .css({ left: "-100%" });
+          // 커버제거
+          cover.hide();
+        });
+        // 슬라이드순번 감소(0보다 작으면 끝번호)
+      sNum--;
+        if(sNum<0) sNum = sCnt-1;
+        // console.log('슬라이드 순번:',sNum);
+      }
+      // 제자리로 ////
+      else {
+        sldBox.animate({ left: "-100%" }, 300, "easeOutQuint", () => {
+          // 커버제거
+          cover.hide();
+        });
+      }
+  
+      bindic.eq(sNum).addClass('on')
+      .siblings().removeClass('on');
+  
+    }); /////////// dragstop /////////
+
+    })
+
 
       // 선택데이터
       const selData = mvData;
@@ -197,13 +201,14 @@ export function Banner(){
       return(
 
         <section className="ats-movie">
-          {/* 이동슬라이드 */}
-          <ul className="ats-movie-area">{makeList(selData)}</ul>
+          <ul className="ats-movie-area">
+          {makeList(selData)}</ul>
+        {/* 이동버튼 + 슬라이드 블릿 : 
+        슬라이드 2개 이상일때 보임 */}
           {
             selData.length > 1 && (
               <>
               {/* 동영상 넘기기 버튼 */}
-              {/* 폰트어썸 아이콘 */}
                 <div className="arrow lar"
                 onClick={goSlide}>
                   <img src="./images/white-arrow.png" alt="" />
@@ -212,9 +217,10 @@ export function Banner(){
                 onClick={goSlide}>
                   <img src="./images/white-arrow.png" alt="" />
                 </div>
+                {/* 인디케이트 버튼 */}
                 <ol className="bindic">
-                  {selData.map((v, i) => (
-                <li className={i == 0 ? "on" : ""} key={i}></li>
+                  {selData.map((v,i) => (
+                <li className={v.ats == "SOMI" ? "on" : ""} key={i}></li>
                 ))}
                 </ol>
                 <div className="cover"></div>
